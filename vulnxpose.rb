296f49b4b83a6f6b1e2ec9ceab666843f62ae191
Rick Flores (nanotechz9l) <0xnanoquetz9l@gmail.com>
2013-10-02 22:09:12 -0700

    added info gathering method, and made output prettier.

--- vulnxpose.rb
+++ vulnxpose.rb
@@ -18,7 +18,7 @@ print """
 :. ## ##::: ##:::: ##: ##::::::: ##:. ###:: ##:. ##:: ##:::::::: ##:::: ##:'##::: ##: ##:::::::
 ::. ###::::. #######:: ########: ##::. ##: ##:::. ##: ##::::::::. #######::. ######:: ########:
 :::...::::::.......:::........::..::::..::..:::::..::..::::::::::.......::::......:::........::
-"""
+""".foreground(:red)
 end
 
 # Kreate the variable text
@@ -38,17 +38,47 @@ if not ARGV[0]
  b()
 end
 
- 
 # User greet!
-#puts "TOP SECRET | Majic Eyes 0NLY".foreground(:red).bright.blink
+puts "\n\tVULNXPOSE v0.1 written by ".foreground(:red) << "Rick <@nanotechz9l> Flores".foreground(:white)
+puts "\tI scan .c source files for known dangerous/banned functions, & calculate an overall security score.".foreground(:red)
 
-# Echo the number of x(lines)
-puts "Analyzing #{line_count} lines from #{ARGV[0]} file.".foreground(:white)
+# Echo the number of lines in source file
+results = []
+words = 0
+chars = 0
+minline = 0
+maxline = 0
 
+# Analyze file information stolen from: http://redneckprogrammer.blogspot.com/2007/09/word-character-line-counter-ruby-script.html
+filename = ARGV[0]
+File.new(filename, "r").each { |line| results << line }
+
+puts "\nStep 1/3:".foreground(:white).underline.bright << " Gathering basic file information from #{filename}... (need to know what im dealing with)".foreground(:cyan)
+puts "#{filename} has the following attributes:"
+puts " -> #{results.size} lines."
+
+results.each do |line|
+ chars += line.length
+ words += line.split.length
+ 
+ if line.length > maxline then
+  maxline = line.length
+ elsif line.length < minline then
+  minline = line.length
+ end
+ 
+end
+puts " -> #{words} words."
+puts " -> #{chars} characters."
+puts " -> #{minline} character shortest line length."
+puts " -> #{maxline} characters longest line length."
+
+# Start analyzing source file for security vulns
 File.open(ARGV[0]) do |f|
   f.each_line do |line|
     if line =~ /sprintf/ # need to add regex support <-member to update dfuns aswell!!
-      puts "\nThe following potentially dangerous methods were found:::".foreground(:red)
+      puts "\nStep 2/3:".foreground(:white).underline.bright << " Starting security analysis on #{filename}...".foreground(:cyan)
+      puts "The following potentially dangerous methods were found:::".foreground(:red)
       puts "sprintf: #{line}".foreground(:white)
     end
   end
@@ -64,5 +94,7 @@ good_words = all_words.select{ |word| !dfuncs.include?(word) }
 good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
 
 #Echo final results to user :::
-puts "#{good_percentage}% of functions are not dangerous" # this now working as expected.
+puts "Step 3/3:".foreground(:white).underline.bright << " Ready to display the overall percentage score for #{filename}.".foreground(:cyan)
+puts "Score guidelines are: <= (less than) 97% horrible code, >= (greater than) 97% secure code.".foreground(:cyan)
+puts "\nFinal security score for #{filename} is" << " #{good_percentage}%".foreground(:red).blink # this now working as expected.
 puts "-- End of analysis\n\n"
