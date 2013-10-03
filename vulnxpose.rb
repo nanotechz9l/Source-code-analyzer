f5d8ef5f7650ca99cc5022b187cf65f6696a3bae
Rick Flores (nanotechz9l) <0xnanoquetz9l@gmail.com>
2013-10-02 21:01:15 -0700

    updated code. still need regex support

--- vulnxpose.rb
+++ vulnxpose.rb
@@ -1,43 +1,25 @@
 #!/usr/bin/env ruby
-
-require 'rainbow';require 'rubygems'
+require 'rainbow'
 
 =begin
 #   :::$Id: analyZer.rb 6191 2011-04-05 0126hrs nano $
 #   :::Source k0de Analyzer koded by Nanotechz9l
 #   :::zet Xterm to 102x24 for better reZUltSss./|
 #   :::$Revision: 1.0.0.1 $
-         _   _                   _____            _                      
-        | \ | |                 |  ___|          | |                     
-        |  \| | __ _ _ __   ___ | |__ _ __   __ _| |_ __   ___  ___ _ __ 
-        | . ` |/ _` | '_ \ / _ \|  __| '_ \ / _` | | '_ \ / _ \/ _ \ '__|
-        | |\  | (_| | | | | (_) | |__| | | | (_| |_| | | |  __/  __/ |   
-        \_| \_/\__,_|_| |_|\___/\____/_| |_|\__, (_)_| |_|\___|\___|_|   
-                                             __/ |                       
-                                            |___/     
-  [~] 0x0, Load file containing the text or       document you want to analyze.
-  [~] 0x1, Put the text into a string and measure its length to get a character count.
-  [~] 0x2, Remove whitespace to get the character count excluding spaces.
-  [~] 0x3, Split out all the whitespace to find out how many words there are.
-  [~] 0x4, Split on full stops to find out how many sentences there are.
-  [~] 0x5, Split on double newlines to find out how many paragraphs there are.
-  [~] 0x6, Perform calculations to work out the averages.
-
-  Copyright (C) 2011  Rick Flores AKA Nanotechz9l | nanotechz9l<v>gmail
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
+
+def b()
+print """
+'##::::'##:'##::::'##:'##:::::::'##::: ##:'##::::'##:'########:::'#######:::'######::'########:
+ ##:::: ##: ##:::: ##: ##::::::: ###:: ##:. ##::'##:: ##.... ##:'##.... ##:'##... ##: ##.....::
+ ##:::: ##: ##:::: ##: ##::::::: ####: ##::. ##'##::: ##:::: ##: ##:::: ##: ##:::..:: ##:::::::
+ ##:::: ##: ##:::: ##: ##::::::: ## ## ##:::. ###:::: ########:: ##:::: ##:. ######:: ######:::
+. ##:: ##:: ##:::: ##: ##::::::: ##. ####::: ## ##::: ##.....::: ##:::: ##::..... ##: ##...::::
+:. ## ##::: ##:::: ##: ##::::::: ##:. ###:: ##:. ##:: ##:::::::: ##:::: ##:'##::: ##: ##:::::::
+::. ###::::. #######:: ########: ##::. ##: ##:::. ##: ##::::::::. #######::. ######:: ########:
+:::...::::::.......:::........::..::::..::..:::::..::..::::::::::.......::::......:::........::
+"""
+end
 
 # Kreate the variable text
 text=''
@@ -47,7 +29,16 @@ text=''
 lines 		= File.readlines(ARGV[0]) # or I could use ARGV.first which is the same thing :)
 line_count 	= lines.size
 text 		= lines.join
+b()
 
+if not ARGV[0]
+ print "\nMISSING input file!".foreground(:red).bright
+ print "\nUsage: #{$0} vulnfile.c\n\n".foreground(:white).bright
+ exit(0)
+ b()
+end
+
+ 
 # User greet!
 #puts "TOP SECRET | Majic Eyes 0NLY".foreground(:red).bright.blink
 
@@ -56,14 +47,14 @@ puts "Analyzing #{line_count} lines from #{ARGV[0]} file.".foreground(:white)
 
 File.open(ARGV[0]) do |f|
   f.each_line do |line|
-    if line =~ /sprintf/
+    if line =~ /sprintf/ # need to add regex support
       puts "\nThe following potentially dangerous methods were found:::".foreground(:red)
       puts "Found sprintf: #{line}".foreground(:white)
     end
   end
 end
 
-# Dangerous functions
+# Dangerous functions // expiremental!
 dfuncs = %w{sprintf strcpy}
 
 # Make a list of words from the input file that aren't dangerous functions,
@@ -73,7 +64,5 @@ good_words 	= all_words.select{ |word| !dfuncs.include?(word) }
 good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
 
 #Echo final results to user :::
-puts "#{good_percentage}% of functions are not dangerous"
-#puts "Summary:\n" + ideal_sentences.join(". ")
-#puts "#{dfuncs}"
+puts "#{good_percentage}% of functions are not dangerous" # this does not seem to be 100% accurate!
 puts "-- End of analysis\n\n"
