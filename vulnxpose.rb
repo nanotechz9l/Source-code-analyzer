f2f836c068ce2f9b40751c1924faf1c01ac589f3
Rick Flores (nanotechz9l) <0xnanoquetz9l@gmail.com>
2013-10-03 12:14:45 -0700

    added regex search

--- vulnxpose.rb
+++ vulnxpose.rb
@@ -8,7 +8,7 @@ require 'rainbow'
 #   :::$Revision: 0.1 $
 =end
 
-def b()
+def v() # TODO: make random at runtime at some point (like msf, and set... no biggie)
 print """
 '##::::'##:'##::::'##:'##:::::::'##::: ##:'##::::'##:'########:::'#######:::'######::'########:
  ##:::: ##: ##:::: ##: ##::::::: ###:: ##:. ##::'##:: ##.... ##:'##.... ##:'##... ##: ##.....::
@@ -22,38 +22,38 @@ print """
 end
 
 # Kreate the variable text
-text=''
+text =''
 
 # File implements a readlines method that reads an entire file into an array line by line 
 # You can use this both to count the lines and join them all into a single string
-lines 		= File.readlines(ARGV[0]) # or I could use ARGV.first which is the same thing :)
-line_count 	= lines.size
-text 		= lines.join
-b()
+lines      = File.readlines(ARGV[0]) # or I could use ARGV.first which is the same thing :)
+line_count = lines.size
+text       = lines.join
+v()
 
 if not ARGV[0]
  print "\nMISSING input file!".foreground(:red).bright
  print "\nUsage: #{$0} vulnfile.c\n\n".foreground(:white).bright
  exit(0)
- b()
+ v()
 end
 
 # User greet!
 puts "\n\tVULNXPOSE v0.1 written by ".foreground(:red) << "Rick <@nanotechz9l> Flores".foreground(:white)
-puts "\tI scan .c source files for known dangerous/banned functions, & calculate an overall security score.".foreground(:red)
+puts "\tI scan .C source files for known dangerous/banned functions, & calculate an overall security score.".foreground(:red)
 
-# Echo the number of lines in source file
+# Analyze file information k0de stolen from: http://redneckprogrammer.blogspot.com/2007/09/word-character-line-counter-ruby-script.html
+# Initialize variables to default values
 results = []
-words = 0
-chars = 0
+words   = 0
+chars   = 0
 minline = 0
 maxline = 0
 
-# Analyze file information stolen from: http://redneckprogrammer.blogspot.com/2007/09/word-character-line-counter-ruby-script.html
 filename = ARGV[0]
 File.new(filename, "r").each { |line| results << line }
 
-puts "\nStep 1/3:".foreground(:white).underline.bright << " Gathering basic file information from #{filename}... (need to know what im dealing with)".foreground(:cyan)
+puts "\nStep 1/3:".foreground(:white).underline.bright << " Gathering basic file information from #{filename}...".foreground(:cyan)
 puts "#{filename} has the following attributes:"
 puts " -> #{results.size} lines."
 
@@ -65,26 +65,29 @@ results.each do |line|
   maxline = line.length
  elsif line.length < minline then
   minline = line.length
+   end
  end
- 
-end
+
 puts " -> #{words} words."
 puts " -> #{chars} characters."
 puts " -> #{minline} character shortest line length."
 puts " -> #{maxline} characters longest line length."
+# -- end of stolen k0de
 
 # Start analyzing source file for security vulns
+# Note that since a block is given, file will automatically be closed when the block terminates
+# TODO: Need to add regex support or ability to scan for multiple strings like 160 of them.
+puts "\nStep 2/3:".foreground(:white).underline.bright << " Starting security analysis on #{filename}...".foreground(:cyan)
 File.open(ARGV[0]) do |f|
   f.each_line do |line|
-    if line =~ /sprintf/ # need to add regex support <-member to update dfuns aswell!!
-      puts "\nStep 2/3:".foreground(:white).underline.bright << " Starting security analysis on #{filename}...".foreground(:cyan)
+    if line.match(/(sprintf|strcpy|strcpyA|scanf)/i)
       puts "The following potentially dangerous methods were found:::".foreground(:red)
-      puts "sprintf: #{line}".foreground(:white)
+      puts "#{line}".foreground(:white)
     end
   end
 end
 
-# Dangerous functions // expiremental!
+# TODO: This must be tied into the above way I use to scan for multiple values vs 1 in order for the % scoring to work!
 dfuncs = %w{sprintf}
 
 # Make a list of words in the text that aren't dangerous,
@@ -93,8 +96,8 @@ all_words = text.scan(/\w+/)
 good_words = all_words.select{ |word| !dfuncs.include?(word) }
 good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
 
-#Echo final results to user :::
+# Echo final results to user :::
 puts "Step 3/3:".foreground(:white).underline.bright << " Ready to display the overall percentage score for #{filename}.".foreground(:cyan)
-puts "Score guidelines are: <= (less than) 97% horrible code, >= (greater than) 97% secure code.".foreground(:cyan)
-puts "\nFinal security score for #{filename} is" << " #{good_percentage}%".foreground(:red).blink # this now working as expected.
+puts "Score guidelines are: <= (less than) 97% horrible code, >= (greater than) 99% secure code.".foreground(:cyan)
+puts "\nFinal security score for \"#{filename}\" is" << " #{good_percentage}%".foreground(:red).blink
 puts "-- End of analysis\n\n"
